09f39c2675261cf3fcf1f9d0b9e4aaa478444e71
unknown <syafar44>
2024-08-08 16:54:51 +0800

    update bag color

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <Navbar />
       <div className="flex justify-center w-full mt-40 lg:mt-16 xl:mt-32 2xl:mt-52 2xl:mb-64 2xl:px-96">
         <div className="px-5">
-          <h1 className="text-center text-6xl text-yellow-500 font-extrabold ">
+          <h1 className="text-center text-6xl text-yellow-700 font-extrabold ">
             TES DISC
           </h1>
           <p className="py-3 font-semibold text-center lg:text-xl">
@@ -18,7 +18,7 @@ export default function Home() {
           </p>
           <div className="flex justify-center mb-2 2xl:mb-7">
             <Link href="/Disc">
-              <button className="btn bg-yellow-500 dark:text-black">Mulai Tes</button>
+              <button className="btn bg-yellow-700 dark:text-black">Mulai Tes</button>
             </Link>
           </div>
         </div>
